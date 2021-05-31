e0a69683eabdf4782cbe0217e24cd0ff5f23430d
Joel Stobart <joel.stobart@wearesnook.com>
2021-06-01 00:01:51 +0100

    Changes based on the supplied data. Dodgy fix for the link based on type.
    Again: apologies - I'm now very tired

--- src/controllers/animals.js
+++ src/controllers/animals.js
@@ -3,6 +3,7 @@ const Animals = require('../services/animals')
 const animals = new Animals(process.env.API_URL)
 
 exports.results = function (req, res) {
-    const data = animals.results(req.query.keyword)
-    res.render('results',{animals: data})
+    animals.results(req.query.keyword).then((data) => {
+        res.render('results', {animals: data.body})
+    });
 }
--- src/services/animals.js
+++ src/services/animals.js
@@ -7,10 +7,7 @@ class Animals extends WebService {
 
 
   results(keyword) {
-    console.log(`${this.url}/results?keyword=${keyword}`);
-
-    //return this.agent.get(`${this.url}/results?keyword=${keyword}`);
-    return [ {name: "joe", description: "tolpuddle", type: "insect", id:"9348390483ldasdkasd"}];
+    return this.agent.get(`${this.url}/results?keyword=${keyword}`);
   }
 
 }
