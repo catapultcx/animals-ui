6cbb750b30b7a9bb6139f6ad97382e78d088279b
Sujith Balakrishnan <sujith.balakrishnan@digital.homeoffice.gov.uk>
2021-08-07 11:15:26 +0100

    1 - Store horse information

---
+++ src/controllers/horses.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const Cats = require('../services/cats')
+const cats = new Cats(process.env.API_URL)
+
+exports.all = function (req, res) {
+  cats.all().then((data) => {
+    res.render('cats', { cats: data })
+  })
+}
+
+exports.get = function (req, res) {
+  cats.get(req.params.id).then((data) => {
+    res.render('view-cat', { cat: data })
+  })
+}
+
+exports.addPage = function (req, res) {
+  res.render('add-cat')
+}
+
+exports.add = function (req, res) {
+  cats.create(req.body).then(() => {
+    res.redirect('/cats')
+  })
+}
+
