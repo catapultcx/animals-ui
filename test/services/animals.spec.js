eea67436cc26344703780119f20532504709711e
llmikeyj <mike.jackson@benezet.co.uk>
2022-12-18 17:11:45 +0000

    1: Store Animal Information: add animal specs

---
+++ test/services/animals.spec.js
@@ -0,0 +1,39 @@
+import { config } from 'dotenv'
+import Cats from '../../src/services/cats.js'
+
+config()
+const service = new Cats(process.env.API_URL)
+let created
+
+let item = { name: 'Tom', description: 'Friend of Jerry', type: 'Cat', colour: 'Grey-ish' }
+
+describe('animals', function () {
+
+  it('get create an animal', function () {
+
+    return service.create(item).then((data) => {
+      expect(data.name).toEqual(item.name)
+      expect(data.description).toEqual(item.description)
+      expect(data.id).toBeDefined()
+      expect(data.type).toEqual(item.type)
+      expect(data.colour).toEqual(item.colour)
+      created = data
+    })
+  })
+
+  it('get a created animal', function () {
+    return service.get(created.id).then((data) => {
+      expect(data.name).toEqual(item.name)
+      expect(data.description).toEqual(item.description)
+      expect(data.type).toEqual(item.type)
+      expect(data.colour).toEqual(item.colour)
+      expect(data.id).toBeDefined()
+    })
+  })
+
+  it('get all animals', function () {
+    return service.all().then((data) => {
+      expect(data.length).toBeGreaterThan(0)
+    })
+  })
+})
---
+++ test/web/animals.spec.js
@@ -0,0 +1,39 @@
+import request from 'supertest'
+import app from '../../src/app'
+
+describe('/', function () {
+
+  it('should get animals', function () {
+    return request(app)
+      .get('/animals')
+      .expect(200)
+      .then(data => {
+        expect(data.text).toContain('Animals')
+      })
+  })
+
+  it('should get an animal', function () {
+    return request(app)
+      .get('/animals/123')
+      .expect(200)
+      .then(data => {
+        expect(data.text).toContain('Animal')
+      })
+  })
+
+  it('should get add animal page', function () {
+    return request(app)
+      .get('/animals/add')
+      .expect(200)
+      .then(data => {
+        expect(data.text).toContain('Add an Animal')
+      })
+  })
+
+  it('should add an Animal', function () {
+    return request(app)
+      .post('/animals')
+      .expect(302)
+      .send({ name: 'Test cat', description: 'Test description', type: 'Test type', colour: 'Test colour' })
+  })
+})
