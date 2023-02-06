88b2def4bb661638349c7d9d2bfdb2b1243b968e
harshvasudev <harsh.vasudev@cyanconnode.com>
2023-02-06 01:24:33 +0000

    Improved test coverage to ~100%

--- test/services/animals.spec.js
+++ test/services/animals.spec.js
@@ -5,7 +5,7 @@ config()
 const service = new Animals(process.env.API_URL)
 let created
 
-let item = { name: 'Tom', description: 'Friend of Jerry' }
+let item = { name: 'Tom', type:'Cat', color:"Brown", description: 'Friend of Jerry' }
 
 describe('animals', function () {
 
@@ -13,6 +13,8 @@ describe('animals', function () {
 
     return service.create(item).then((data) => {
       expect(data.name).toEqual(item.name)
+      expect(data.type).toEqual(item.type)
+      expect(data.color).toEqual(item.color)
       expect(data.description).toEqual(item.description)
       expect(data.id).toBeDefined()
 
@@ -26,6 +28,8 @@ describe('animals', function () {
   it('get a created animal', function () {
     return service.get(created.id).then((data) => {
       expect(data.name).toEqual(item.name)
+      expect(data.type).toEqual(item.type)
+      expect(data.color).toEqual(item.color)
       expect(data.description).toEqual(item.description)
       expect(data.id).toBeDefined()
 
@@ -40,4 +44,46 @@ describe('animals', function () {
       expect(data.length).toBeGreaterThan(0)
     })
   })
+
+  it('update a created animal', function () {
+    let itemToUpdate = { id:created.id, name: 'Bravo', type:'Dog', color:"Brown", description: 'Friend of Jerry' }
+    return service.update(itemToUpdate).then((data) => {
+      expect(data).toEqual({})
+    })
+  })
+
+  it('get an updated animal', function () {
+    return service.get(created.id).then((data) => {
+      expect(data.name).toEqual('Bravo')
+      expect(data.type).toEqual('Dog')
+      expect(data.color).toEqual('Brown')
+      expect(data.description).toEqual('Friend of Jerry')
+      expect(data.id).toBeDefined()
+    })
+  })
+
+  it('filter an animal', function () {
+    return service.filter('Garfield', 'Cat', 'Grey','Lazy cat').then((data) => {
+      expect(data.length).toBeGreaterThan(0)
+      expect(data[0].name).toEqual('Garfield')
+      expect(data[0].type).toEqual('Cat')
+      expect(data[0].color).toEqual('Grey')
+      expect(data[0].description).toEqual('Lazy cat')
+    })
+  })
+
+  it('delete a created animal', function () {
+    let itemToUpdate = { id:created.id, name: 'Bravo', type:'Dog', color:"Brown", description: 'Friend of Jerry' }
+    return service.delete(created.id).then((data) => {
+      expect(data).toEqual({})
+    })
+  })
+
+  it('filter a created animal', function () {
+    let itemToUpdate = { id:created.id, name: 'Bravo', type:'Dog', color:"Brown", description: 'Friend of Jerry' }
+    return service.delete(created.id).then((data) => {
+      expect(data).toEqual({})
+    })
+  })
+
 })
--- test/web/animals.spec.js
+++ test/web/animals.spec.js
@@ -2,38 +2,81 @@ import request from 'supertest'
 import app from '../../src/app'
 
 describe('/', function () {
+    let createdId;
 
-  it('should get animals', function () {
-    return request(app)
-      .get('/animals')
-      .expect(200)
-      .then(data => {
-        expect(data.text).toContain('Animals')
-      })
-  })
+    it('should get animals', function () {
+        return request(app)
+            .get('/animals')
+            .expect(200)
+            .then(data => {
+                expect(data.text).toContain('Animals')
+                let startPos = data.text.indexOf("/animals/update/") + 16;
+                createdId = data.text.substring(startPos, startPos + 36);
+            })
+    })
 
-  it('should get a animal', function () {
-    return request(app)
-      .get('/animals/123')
-      .expect(200)
-      .then(data => {
-        expect(data.text).toContain('Animal')
-      })
-  })
+    it('should get a animal', function () {
+        return request(app)
+            .get('/animals/123')
+            .expect(200)
+            .then(data => {
+                expect(data.text).toContain('Animal')
+            })
+    })
 
-  it('should get add animal page', function () {
-    return request(app)
-      .get('/animals/add')
-      .expect(200)
-      .then(data => {
-        expect(data.text).toContain('Add a Animal')
-      })
-  })
+    it('should get add animal page', function () {
+        return request(app)
+            .get('/animals/add')
+            .expect(200)
+            .then(data => {
+                expect(data.text).toContain('Add a Animal')
+            })
+    })
 
-  it('should add a animal', function () {
-    return request(app)
-      .post('/animals')
-      .expect(302)
-      .send({ name: 'Test animal', description: 'Test description' })
-  })
+    it('should add a animal', function () {
+        return request(app)
+            .post('/animals')
+            .expect(302)
+            .send({name: 'Test animal', type: 'Dog', color: 'Grey', description: 'Test description'})
+    })
+
+    it('should get update animal page', function () {
+        return request(app)
+            .get('/animals/update/id')
+            .expect(200)
+            .then(data => {
+                expect(data.text).toContain('Update an Animal')
+            })
+    })
+
+    it('should update an animal', function () {
+        return request(app)
+            .post('/animals/update')
+            .expect(302)
+            .send({id: createdId, name: 'Test animal', type: 'Dog', color: 'Brown', description: 'Test description'})
+    })
+
+    it('should delete an animal', function () {
+        return request(app)
+            .get('/animals/delete/id')
+            .expect(302)
+            .then(data => {
+                expect(data.text).toContain('Found. Redirecting to /animals')
+            })
+    })
+
+    it('should get filter animal page', function () {
+        return request(app)
+            .get('/animals/filter')
+            .expect(200).then(data => {
+                expect(data.text).toContain('Filter Animals')
+            })
+    })
+
+    it('should filter an animal', function () {
+        return request(app)
+            .post('/animals/filter')
+            .expect(200)
+    })
+
 })
