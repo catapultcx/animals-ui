38e911ef7833ae8ca7f4fb6cbec85aa32199dd20
siva.prakash <7499500+sivaprakashiv@users.noreply.github.com>
2023-03-15 21:25:10 +0000

    Increase test coverage for animals api integration

---
+++ test/services/animals.spec.js
@@ -0,0 +1,40 @@
+import { config } from 'dotenv'
+import Animals from '../../src/services/animals.js'
+
+config()
+const service = new Animals("http://localhost:8080/api/1")
+let created
+
+let animal = { name: 'Carey', description: 'a caring cow', color: 'brown', type: 'MAMMALS' }
+
+describe('animals', function () {
+
+  it('post create an animal', function () {
+
+    return service.create(animal).then((data) => {
+      expect(data.id).toBeDefined()
+      expect(data.name).toEqual(animal.name)
+      expect(data.description).toEqual(animal.description)
+      expect(data.color).toEqual(animal.color)
+      expect(data.type).toEqual(animal.type)
+
+      created = data
+    })
+  })
+
+  it('get a created animal', function () {
+    return service.get(created.id).then((data) => {
+      expect(data.name).toEqual(animal.name)
+      expect(data.description).toEqual(animal.description)
+      expect(data.id).toBeDefined()
+      expect(data.color).toEqual(animal.color)
+      expect(data.type).toEqual(animal.type)
+    })
+  })
+
+  it('get all animals', function () {
+    return service.all().then((data) => {
+      expect(data.length).toBeGreaterThan(0)
+    })
+  })
+})
